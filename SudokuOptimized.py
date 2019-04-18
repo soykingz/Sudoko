2d3db8ba3ab9a778dd66a7c3b495babc61522398
soykingz <soykingz@users.noreply.github.com>
2019-04-18 12:24:59 -0400

    sudoku

--- SudokuOptimized.py
+++ SudokuOptimized.py
@@ -68,45 +68,54 @@ class Sudoku:
         return True
 
     @staticmethod
-    def makeImplications(grid, i, j, value):
+    def makeImplications(grid, i, j, e):
         '''(list of (list of int), int, int, int) -> list of (int, int, int)
         '''
         global sectors
-        grid[i][j] = value
-        impl = [(i, j, value)]
-        for cur_sect in range(len(sectors)):
-            sectInfo = []
-            # find missing element in cur_sect sectors
-            validSet = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-            for x in range(sectors[cur_sect][0], sectors[cur_sect][1]):
-                for y in range(sectors[cur_sect][2], sectors[cur_sect][3]):
+    
+        grid[i][j] = e
+        impl = [(i, j, e)]
+    
+    
+        for k in range(len(sectors)):
+    
+            sectinfo = []
+    
+            #find missing elements in ith sector
+            vset = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+            for x in range(sectors[k][0], sectors[k][1]):
+                for y in range(sectors[k][2], sectors[k][3]):
                     if grid[x][y] != 0:
-                        validSet.remove(grid[x][y])
-            # attach copy of validSet to each missing square in sect sector
-            for x in range(sectors[cur_sect][0], sectors[cur_sect][1]):
-                for y in range(sectors[cur_sect][2], sectors[cur_sect][3]):
-                    if grid[x][y] != 0:
-                        sectInfo.append([x, y, validSet.copy()])
-            for m in range(len(sectInfo)):
-                curSectInfo = sectInfo[m]
-                # find the set of elements on the row corresponding to m 
-                # and remove them
+                        vset.remove(grid[x][y])
+    
+            #attach copy of vset to each missing square in ith sector
+            for x in range(sectors[k][0], sectors[k][1]):
+                for y in range(sectors[k][2], sectors[k][3]):
+                    if grid[x][y] == 0:
+                        sectinfo.append([x, y, vset.copy()])
+            
+            for m in range(len(sectinfo)):
+                sin = sectinfo[m]
+                
+                #find the set of elements on the row corresponding to m and remove them
                 rowv = set()
-                for y in range(0, 9):
-                    rowv.add(grid[curSectInfo[0]][y])
-                valLeft  = curSectInfo[2].difference(rowv)
-                # find the set of elements on the column and remove them
+                for y in range(9):
+                    rowv.add(grid[sin[0]][y])
+                left = sin[2].difference(rowv)
+                
+                #find the set of elements on the column corresponding to m and remove them
                 colv = set()
-                for x in range(0, 9):
-                    colv.add(grid[x][curSectInfo[1]])
-                valLeft = valLeft.difference(colv)
-                # check if theres only one value left in vset
-                if len(valLeft) == 1:
-                    value = valLeft.pop()
-                    if Sudoku.is_valid(grid, curSectInfo[0], curSectInfo[1],
-                                       value):
-                        grid[curSectInfo[0]][curSectInfo[1]] = value
-                        impl.append((curSectInfo[0], curSectInfo[1], value))
+                for x in range(9):
+                    colv.add(grid[x][sin[1]])
+                left = left.difference(colv)
+                             
+                #check if the vset is a singleton
+                if len(left) == 1:
+                    val = left.pop()
+                    if Sudoku.is_valid(grid, sin[0], sin[1], val):
+                        grid[sin[0]][sin[1]] = val
+                        impl.append((sin[0], sin[1], val))
+                    
         return impl
 
     def undoImplications(grid, impl):
